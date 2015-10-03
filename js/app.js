9dc31120791600f3495a03a9be2a11bbeb479404
fredyteheranto <fredyteheran91@gmail.com>
2015-10-03 15:36:22 -0500

    angular inicio proyecto catalogo

---
+++ js/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('noviaapp',[ ]);
+app.controller('profesorCtrl', function () {
+
+	this.mynovia = noviaData;
+	
+});
+
+var noviaData = {
+
+	nombre: "Maria alejandra",
+	bio: "estudiante",
+	edad:  17,
+	foto: "img/novia.jpg",
+	tamano: 200
+}
